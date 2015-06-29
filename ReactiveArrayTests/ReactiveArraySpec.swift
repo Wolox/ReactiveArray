75f06b779a6963e368d2e924dbde61afe0dc362c
Guido Marucci Blas <guidomb@gmail.com>
2015-06-29 19:10:06 -0300

    Adds Quick & Nimble for testing.

---
+++ ReactiveArrayTests/ReactiveArraySpec.swift
@@ -0,0 +1,18 @@
+//
+//  ReactiveArraySpec.swift
+//  ReactiveArraySpec
+//
+//  Created by Guido Marucci Blas on 6/29/15.
+//  Copyright (c) 2015 Wolox. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+class ReactiveArraySpec: QuickSpec {
+    
+    override func spec() {
+        
+    }
+    
+}

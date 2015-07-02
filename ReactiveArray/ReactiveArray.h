94b89d78afaa08a785b98e0fc57a92e49285d21e
Guido Marucci Blas <guidomb@gmail.com>
2015-07-02 00:19:31 -0300

    Removes UIKit dependency.

--- ReactiveArray/ReactiveArray.h
+++ ReactiveArray/ReactiveArray.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2015 Wolox. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for ReactiveArray.
 FOUNDATION_EXPORT double ReactiveArrayVersionNumber;
